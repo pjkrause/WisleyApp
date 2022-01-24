cea6b92bb32e3246f9bd1f1d69531e8be53d3134
Paul Krause <paulkrause@Pauls-iMac.local>
2022-01-24 12:32:41 +0000

    usual hassles

--- client/serviceworker.js
+++ client/serviceworker.js
@@ -1,11 +1,11 @@
 // This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
-
+const version = 'V0.1';
 const CACHE = "pwabuilder-offline-page";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "https://intense-woodland-96442.herokuapp.com/offline.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
