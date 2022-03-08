d247960c2071abf46b995630f928f755cfef4621
Paul Krause <paulkrause@Pauls-iMac.local>
2022-03-08 12:49:32 +0000

    WorkBox Service worker added

---
+++ workbox-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	globDirectory: 'client/',
+	globPatterns: [
+		'**/*.{png,html,json,js}'
+	],
+	swDest: 'client/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	],
+
+	// Define runtime caching rules.
+  	runtimeCaching: [{
+    	// Match any request that ends with .png, .jpg, .jpeg or .svg.
+    	urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+    	// Apply a cache-first strategy.
+    	handler: 'CacheFirst',
+
+    	options: {
+     	 // Use a custom cache name.
+      	cacheName: 'images',
+
+      	// Only cache 10 images.
+      	expiration: {
+        	maxEntries: 10,
+      	},
+    	},
+  	}],
+};
